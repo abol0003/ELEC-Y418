14e90bc25b7bfcfda80d674afd6641325297d030
abol0003 <95567171+abol0003@users.noreply.github.com>
2025-04-03 12:42:04 +0200

    need to show something on the screen

--- Project/Project/main.asm
+++ Project/Project/main.asm
@@ -1,257 +1,351 @@
+;------------------------------------------------------------
+; Code complet corrigé pour l’affichage du pixel du serpent et
+; pour la transmission du buffer d’affichage vers l’écran
+;------------------------------------------------------------
+
 .INCLUDE "M328PDEF.INC"
 
 .CSEG
-.ORG 0X0000
+.ORG 0x0000
 RJMP init
+
 .ORG 0x001A
-RJMP TIMER1_OVF
+;RJMP TIMER1_OVF
 
-; ------------------------------------------------
+.ORG 0x0020
+RJMP TIMER0_OVF
+
+;------------------------------------------------------------
 ; Initialisation
-; ------------------------------------------------
+;------------------------------------------------------------
 init:
-	LDI R16, HIGH(RAMEND)
-	OUT SPH, R16
-	LDI R16, LOW(RAMEND)
-	OUT SPL, R16
+    ; Initialisation de la pile
+    LDI R16, HIGH(RAMEND)
+    OUT SPH, R16
+    LDI R16, LOW(RAMEND)
+    OUT SPL, R16
 
-	SBI DDRB, 3
-	SBI DDRB, 4
-	SBI DDRB, 5
-	CBI PORTB, 3
-	CBI PORTB, 4
-	CBI PORTB, 5
+    ; Configuration des ports PB3, PB4 et PB5 en sortie
+    LDI R17, (1<<3)|(1<<4)|(1<<5)  ; Create a mask for bits 3, 4, and 5 (binary: 0011 1000)
+    OUT PORTB, r17           ; Initialize these pins (set output state)
+    OUT DDRB, r17            ; Set PB3, PB4, and PB5 as outputs
+	; Configure PC2 comme sortie et initialise à 0 (LED éteinte)
+	SBI DDRC, 2          ; Puis configurer PC2 en sortie
+	SBI PORTC, 2         ; D’abord mettre à 0
 
-	LDI ZL, low(0x0100)
-	LDI ZH, high(0x0100)
-	LDI R16, 0
-	LDI R17, 70
-	RCALL InitScreenBuffer
+    LDI R18, 0           ; Initialiser le compteur de ligne (pour le rafraîchissement)
 
-	LDI R16, 0
-	STS TCCR1A, R16
-	LDI R16, (1<<CS11)|(1<<CS10)
-	STS TCCR1B, R16
-	LDI R16, (1<<TOIE1)
-	STS TIMSK1, R16
-	SEI
+    ; Configuration du Timer1 (pour la logique du jeu – inactif pour l’instant)
+    LDI R16, 0
 
-	RJMP MAINLOOP
+    STS TCCR1A, R16
+    LDI R16, (1<<CS11)|(1<<CS10)   ; Prescaler clk/64
+    STS TCCR1B, R16
+    LDI R16, (1<<TOIE1)
+    STS TIMSK1, R16
 
-; ------------------------------------------------
-; Interruption Timer1
-; ------------------------------------------------
-TIMER1_OVF:
-	PUSH R0
-	PUSH R1
-	PUSH R16
-	PUSH R17
-	PUSH R18
-	PUSH R19
-	PUSH ZL
-	PUSH ZH
+    ; Configuration du Timer0 pour l’affichage
+    LDI R16, (1<<CS01)|(1<<CS00)  
+    OUT TCCR0B, R16
+	LDI R16, (1<<CS01)|(1<<CS00)   
+    LDI R16, 56                    ; Valeur initiale pour TCNT0
+    OUT TCNT0, R16
+    LDI R16, (1<<TOIE0)
+    STS TIMSK0, R16
 
-	RCALL DisplayLine
-	INC R18
-	CPI R18, 7
-	BRLO TimerDone
-	LDI R18, 0
-TimerDone:
-	POP ZH
-	POP ZL
-	POP R19
-	POP R18
-	POP R17
+    SEI
+	RCALL ClearScreen      ; Effacer l'écran
+    RCALL SnakeInit        ; Initialiser le serpent (affichage du pixel)
+    RJMP MAINLOOP
+
+;------------------------------------------------------------
+; SnakeInit : Initialisation du serpent
+; Positionne le pixel du serpent sur la ligne 1 et la colonne 39
+;------------------------------------------------------------
+SnakeInit:
+    LDI snake_row, 2     ; Ligne 1
+    LDI snake_col, 39    ; Colonne 39
+    RCALL SetScreenBit   ; Allume le pixel correspondant dans le buffer
+    RET
+;------------------------------------------------------------
+; SetScreenBit : Allume le pixel à la position (R20, R21)
+;------------------------------------------------------------
+SetScreenBit:
+    PUSH YL
+    PUSH YH
+    RCALL GetByteAndMask   ; R0 contient l’octet actuel, R1 le masque du pixel
+    OR R0, R1            ; do or to keep previous led on on ( for example obstacle etc)
+    ST Y, R0             ; Écrit l’octet mis à jour dans le buffer
+    POP YH
+    POP YL
+    RET
+;------------------------------------------------------------
+; 
+;	Is used to find the position of the snake
+;
+;------------------------------------------------------------
+GetByteAndMask:
+    PUSH R16
+    PUSH R2
+    PUSH R3
+    ; Sauvegarder les coordonnées dans R2 (ligne) et R3 (colonne)
+    MOV R2, R20
+    MOV R3, R21
+    ; Initialiser le pointeur Y vers le début du buffer (0x0100)
+    LDI YL, low(0x0100)
+    LDI YH, high(0x0100)
+    ; Pour chaque ligne, avancer de 5 octets (chaque ligne = 40 colonnes = 5 octets)
+GetByteAndMaskRow:
+    TST R2
+    BREQ GetByteAndMaskP2 ; branch if R2=0
+    ADIW Y, 5 ; enable to select the good row
+    DEC R2
+    RJMP GetByteAndMaskRow
+GetByteAndMaskP2:
+    LDI R16, 8    ; Chaque octet représente 8 colonnes
+GetByteAndMaskCol:
+    CP R3, R16 ; is doing colnum/8
+    BRLO GetByteAndMaskP3	;branch if lower
+    SUB R3, R16
+    ADIW Y, 1 ; max 5 times as only 5 octet in a row
+    RJMP GetByteAndMaskCol
+GetByteAndMaskP3:; ici on est dans la bonne adress de la ram
+    LDI R16, 0b10000000  ; Masque initial pour la colonne 0
+GetByteAndMaskColMask:
+    TST R3 ; ici le reste <8 determinera dans quelle colonne on est 
+    BREQ GetByteAndMaskEnd
+    LSR R16             ; Décalage vers la droite
+    DEC R3
+    RJMP GetByteAndMaskColMask
+GetByteAndMaskEnd:
+    LD R0, Y            ;  lit le contenu de la mémoire pointée par Y
+    MOV R1, R16         ; Copier le masque dans R1 de la colonne
+    POP R3
+    POP R2
+    POP R16
+    RET
+
+;------------------------------------------------------------
+; DisplayLine : Affiche une ligne du buffer sur l’écran
+;------------------------------------------------------------
+
+ClearScreen:
+    PUSH ZL
+    PUSH ZH
+    PUSH R16
+    PUSH R17
+    LDI ZL, low(0x0100)
+    LDI ZH, high(0x0100)
+    LDI R16, 0       ; Valeur zéro pour effacer
+    LDI R17, 70      ; Nombre d’octets à effacer
+clear_loop:
+    ST Z+, R16      ; Écrire 0 dans le buffer et incrémenter le pointeur
+    DEC R17
+    BRNE clear_loop
+
 	POP R16
-	POP R1
-	POP R0
+    POP R17
+    POP ZH
+    POP ZL
+    RET
+DisplayLine:
+	PUSH	R0
+	PUSH	R1
+	PUSH	R16
+	PUSH	R17
+	PUSH	R20
+	PUSH	ZL
+	PUSH	ZH
+	IN		R16,	SREG
+	PUSH	R16
+
+	LDI		ZH,		high(0x0100)
+	LDI		ZL,		low(0x0100)
+
+	LDI		R16,	5
+	MUL		R16,	R24
+	ADD		ZL,		R0
+nbByte_line:
+	LDI R16, 10 ;number of byte to send at same time
+DisplayLineLoop:
+	LD		R20,	Z+
+	CALL	pushByte
+
+	CPI		R16,	6
+	BRNE	HighScreen
+
+	ADIW	Z,		6*5
+
+	SBI		PINB,	4
+
+HighScreen:
+	DEC		R16
+	BRNE	DisplayLineLoop
+	LDI		R20,	0x80
+	; the goal is to transform the column number into binaire
+	SBRC	R24,	2 ; if bit 2 is 0 then Swap is skipped
+	SWAP	R20
+
+	SBRC	R24,	0
+	LSR		R20
+
+	SBRS	R24,	1
+	RJMP	EnableLine
+	
+	LSR		R20
+	LSR		R20
+
+EnableLine:
+	CALL	pushByte
+	SBI		PINB,	4
+	DEC		R24
+	BRGE	endScreen
+	LDI		R24,	6
+
+endScreen:
+	POP		R16
+	OUT		SREG,	R16
+	POP 	ZH
+	POP 	ZL
+	POP 	R20
+	POP		R17
+	POP 	R16
+	POP		R1
+	POP		R0
 	RETI
 
-; ------------------------------------------------
-; Initialiser le buffer
-; ------------------------------------------------
-InitScreenBuffer:
-	ST Z+, R16
-	DEC R17
-	BRNE InitScreenBuffer
-	RCALL SnakeInit
-	LDI R18, 0
+pushByte:
+	LDI		R17,	8
+pushByteLoop:
+	CBI		PORTB,	3
+	BST		R20,	0 ; prend le bit numero b de R20 et le met dans le bit T
+	BRTC	send			;Branch if T Flag Cleared
+	SBI		PORTB,	3
+send:
+	SBI		PINB,	5
+	SBI		PINB,	5
+	LSR		R20 ; decal vers la droite le bit 0 est mis dans le carry
+	DEC		R17
+	BRNE	pushByteLoop
 	RET
+;------------------------------------------------------------
+; TIMER0_OVF : Routine d'interruption du Timer0 pour actualiser l’affichage
+;
+; Appelle DisplayLine pour afficher la ligne courante, puis
+; incrémente le compteur de ligne (R18) et réinitialise TCNT0.
+;------------------------------------------------------------
+TIMER0_OVF:
+    CBI PORTC, 2           ;  Toggle PB2 here to verify interrupt is firing   
+    PUSH R0
+    PUSH R16
+    PUSH R17
+    PUSH R18
+    PUSH R19
+    PUSH ZL
+    PUSH ZH
 
-; ------------------------------------------------
-; SnakeInit : Allume une ligne entière
-; ------------------------------------------------
-SnakeInit:
-	LDI R16, 0       ; ligne 0
-	LDI R17, 0       ; colonne de départ
-	MOV R2, R16
-	LDI R20, 40      ; 40 colonnes = 5 octets
-SetLineLoop:
-	MOV R3, R17
-	RCALL SetScreenBit
-	INC R17
-	DEC R20
-	BRNE SetLineLoop
-	RET
+    RCALL DisplayLine   ; Affiche la ligne actuelle du buffer
 
-; ------------------------------------------------
-; Allumer un pixel à (R2, R3)
-; ------------------------------------------------
-SetScreenBit:
-	PUSH YL
-	PUSH YH
-	RCALL GetByteAndMask
-	OR R0, R1
-	ST Y, R0
-	POP YH
-	POP YL
-	RET
+    INC R18
+    CPI R18, 8
+    BRLO skipReset
+    LDI R18, 0
+skipReset:
+    LDI R16, 56
+    OUT TCNT0, R16
 
-; ------------------------------------------------
-; Éteindre un pixel à (R2, R3)
-; ------------------------------------------------
-ClearScreenBit:
-	PUSH R16
-	PUSH YL
-	PUSH YH
-	RCALL GetByteAndMask
-	LDI R16, 0xFF
-	EOR R1, R16
-	AND R0, R1
-	ST Y, R0
-	POP YH
-	POP YL
-	POP R16
-	RET
+    POP ZH
+    POP ZL
+    POP R19
+    POP R18
+    POP R17
+    POP R16
+    POP R0
+    RETI
 
-; ------------------------------------------------
-; Calcule l'adresse et le mask pour un pixel
-; Entrée : R2 = ligne, R3 = colonne
-; Sortie : R0 = contenu actuel du byte, R1 = mask, Y = adresse du byte
-; ------------------------------------------------
-GetByteAndMask:
-	PUSH R16
-	PUSH R2
-	PUSH R3
+;------------------------------------------------------------
+; TIMER1_OVF : Routine d'interruption du Timer1 (logique du jeu active)
+; Cette routine vérifie la variable "direction" et appelle la routine
+; de déplacement correspondante.
+;------------------------------------------------------------
+TIMER1_OVF:
+    PUSH R16
+    PUSH R17
+    PUSH R18
+    PUSH R19
+    PUSH YL
+    PUSH YH
 
-	LDI R16, 10
-	MUL R2, R16
-	LDI YL, low(0x0100)
-	LDI YH, high(0x0100)
-	ADD YL, R0
-	ADC YH, R1
+    ; Vérification de la direction (les valeurs sont arbitraires et doivent
+    ; correspondre à la logique de votre jeu)
+    LDI R16, 6            ; Valeur pour la direction "droite"
+    CP direction, R16
+    BRNE notRight
+    RCALL moveRight
+    RJMP timer1_end
 
-	LDI R16, 8
-ColByteLoop:
-	CP R3, R16
-	BRLO GotColByte
-	SUB R3, R16
-	ADIW YL, 1
-	RJMP ColByteLoop
+notRight:
+    LDI R16, 4            ; Valeur pour la direction "gauche"
+    CP direction, R16
+    BRNE notLeft
+    RCALL moveLeft
+    RJMP timer1_end
 
-GotColByte:
-	LDI R16, 0b10000000
-BitMaskLoop:
-	TST R3
-	BREQ MaskReady
-	LSR R16
-	DEC R3
-	RJMP BitMaskLoop
+notLeft:
+    LDI R16, 8            ; Valeur pour la direction "haut"
+    CP direction, R16
+    BRNE notUp
+    RCALL moveUp
+    RJMP timer1_end
 
-MaskReady:
-	LD R0, Y
-	MOV R1, R16
-	POP R3
-	POP R2
-	POP R16
-	RET
+notUp:
+    LDI R16, 2            ; Valeur pour la direction "bas"
+    CP direction, R16
+    BRNE timer1_end
+    RCALL moveDown
 
-; ------------------------------------------------
-; Boucle principale
-; ------------------------------------------------
+timer1_end:
+    POP YH
+    POP YL
+    POP R19
+    POP R18
+    POP R17
+    POP R16
+    RETI
+
+;------------------------------------------------------------
+; Définitions pour les variables de déplacement du serpent
+;------------------------------------------------------------
+.DEF direction = R25     ; Direction : 6 = droite, 4 = gauche, 8 = haut, 2 = bas
+.DEF snake_row = R20     ; Coordonnée de la ligne du serpent
+.DEF snake_col = R21     ; Coordonnée de la colonne du serpent
+
+;------------------------------------------------------------
+; Routines de déplacement du serpent
+;------------------------------------------------------------
+moveRight:
+    INC snake_col           ; Augmenter la colonne (déplacement à droite)
+    RCALL SetScreenBit      ; Allumer le pixel à la nouvelle position
+    RET
+
+moveLeft:
+    DEC snake_col           ; Diminuer la colonne (déplacement à gauche)
+    RCALL SetScreenBit
+    RET
+
+moveUp:
+    DEC snake_row           ; Diminuer la ligne (déplacement vers le haut)
+    RCALL SetScreenBit
+    RET
+
+moveDown:
+    INC snake_row           ; Augmenter la ligne (déplacement vers le bas)
+    RCALL SetScreenBit
+    RET
+
+;------------------------------------------------------------
+; MAINLOOP : Boucle principale
+; La gestion du jeu et de l’affichage est assurée par les interruptions.
+;------------------------------------------------------------
 MAINLOOP:
-	RJMP MAINLOOP
-
-; ------------------------------------------------
-; Affiche UNE ligne du buffer
-; Entrée : R18 = ligne
-; ------------------------------------------------
-DisplayLine:
-	LDI ZL, low(0x0100)
-	LDI ZH, high(0x0100)
-	LDI R20, 10
-	MUL R18, R20
-	ADD ZL, R0
-	ADC ZH, R1
-
-	; --- Afficher les 40 colonnes du haut (octets 5 à 9) ---
-	LDI R19, 5
-	ADIW ZL, 5
-DisplayUpper:
-	LD R16, Z+
-	LDI R17, 8
-SendUpperBits:
-	ROL R16
-	BRCC SendZeroUpper
-	SBI PORTB, 3
-	RJMP ClockUpperBit
-SendZeroUpper:
-	CBI PORTB, 3
-ClockUpperBit:
-	SBI PORTB, 5
-	CBI PORTB, 5
-	DEC R17
-	BRNE SendUpperBits
-	DEC R19
-	BRNE DisplayUpper
-
-	; --- Afficher les 40 colonnes du bas (octets 0 à 4) ---
-	LDI R19, 5
-	SBIW ZL, 10
-DisplayLower:
-	LD R16, Z+
-	LDI R17, 8
-SendLowerBits:
-	ROL R16
-	BRCC SendZeroLower
-	SBI PORTB, 3
-	RJMP ClockLowerBit
-SendZeroLower:
-	CBI PORTB, 3
-ClockLowerBit:
-	SBI PORTB, 5
-	CBI PORTB, 5
-	DEC R17
-	BRNE SendLowerBits
-	DEC R19
-	BRNE DisplayLower
-
-	; --- Activer la ligne ---
-	LDI R16, 0b10000000  ; bit de poids fort pour ligne 0
-DisplayLineShift:
-	CPI R18, 0
-	BREQ SkipLineShift
-LSRLine:
-	LSR R16
-	DEC R18
-	BRNE LSRLine
-SkipLineShift:
-	LDI R17, 8
-SendRowBits:
-	LSR R16
-	BRCC SendZeroRow
-	SBI PORTB, 3
-	RJMP ClockRowBit
-SendZeroRow:
-	CBI PORTB, 3
-ClockRowBit:
-	SBI PORTB, 5
-	CBI PORTB, 5
-	DEC R17
-	BRNE SendRowBits
-
-	SBI PORTB, 4
-	NOP
-	CBI PORTB, 4
-	RET
-
+    RJMP MAINLOOP
